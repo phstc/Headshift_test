81c084299f66e4d02ca57ab0393fdcb013dee141
Pablo <pablohstc@gmail.com>
2011-05-03 21:55:33 +0100

    Adding rakefile

---
+++ Rakefile
@@ -0,0 +1,14 @@
+require 'rake'
+require 'rspec/core/rake_task'
+require 'yaml'
+require File.dirname(__FILE__) + '/app/main.rb'
+
+
+desc "Run all specs"
+RSpec::Core::RakeTask.new(:default) do |spec|
+  main = Main.new
+  main.show_questions
+  main.serialize
+  spec.pattern = 'spec/*_spec.rb'
+  # spec.rspec_opts = ['--backtrace']
+end
--- app/main.rb
+++ app/main.rb
@@ -1,44 +1,47 @@
-class Main
-  @@answers = {}
-  
-  def self.show_questions
-    self.methods.each do | method |
+class Main  
+  def show_questions
+    @answers = {}
+    public_methods.each do | method |
         if method.match(/^ask_/)
-          @@answers[method] = eval("self.#{method}")
+          @answers[method] = eval("self.#{method}") == 'yes'
         end
     end
   end
   
-  def self.answers
-    @@answers
+  def answers
+    @answers
   end
   
-  private 
-  
-  def self.ask_hire_me
-    puts "Will Headshift hire me? [yes|no]"
+  def ask_hire_me
+    puts '****************************************'
+    puts ' Will Headshift hire me? [yes|no]'
+    puts '****************************************'
     gets.chomp
   end
   
-  def self.ask_lots_of_money
-    puts "Will Headshift pay me a lots of money to make me rich? [yes|no]"
+  def ask_lots_of_money
+    puts '****************************************'
+    puts ' Will Headshift pay me a lots of money to make me rich? [yes|no]'
+    puts '****************************************'
     gets.chomp
   end
   
-  def self.ask_patience_with_my_english_level
-    puts "Will Headshift be patient with my English level? [yes|no]"
+  def ask_patience_with_my_english_level
+    puts '****************************************'
+    puts ' Will Headshift be patient with my English level? [yes|no]'
+    puts '****************************************'
     gets.chomp
+  end
+  
+  def serialize
+    File.open('main.yaml', 'w') do |out|
+      YAML.dump(self, out )
+    end
+  end
+  
+  def self.unserialize
+    main_file = File.open('main.yaml')
+    YAML::load(main_file)
   end
 
-end
-
-if __FILE__ == $PROGRAM_NAME
-   Main.show_questions
-   # I spent a long time finding how to run rspec from ruby code 
-   # like test/unit - Test::Unit::UI::Console::TestRunner.run(MySuite.new(MyTestCase))
-   # Unfortunately, I didn't find it
-   
-   # My idea here was to create a funny code. Similar this one https://github.com/praekelt/careers/raw/master/software_engineer.py
-   # After the prompt I wanted to run the test suite ../spec/main_spec.rb
-   
 end
